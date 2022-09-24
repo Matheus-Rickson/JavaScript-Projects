91d59a4f498733ed78547080b4ef0e9bad865524
Matheus-Rickson <Contato.rickson@outlook.com>
2022-09-24 16:47:03 -0300

    Primeiro exercicio do curso de js.

---
+++ Exercise 1/YourPlayground.js
@@ -0,0 +1,18 @@
+console.log('hello world')
+console.log('matheus Rickson')
+
+// Bloco de variaveis -> comentarios
+let name = 'Mahteus Rickson'
+console.log(name)
+
+sentence = 'Coe truta, como cê tá? Excelente dia pra você'
+console.log(sentence)
+
+//operadores
+
+food = prompt('Quanto custa a refeição?: ')
+tipPercentage = 0.2
+
+tipAmount = food * tipPercentage
+
+console.log(tipAmount)
